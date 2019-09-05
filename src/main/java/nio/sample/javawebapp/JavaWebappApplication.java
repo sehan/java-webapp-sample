26a511fb2ed37fa008bb0954cfb4019d0b90cfa6
nio.d <nio.d@kakaocorp.com>
2019-09-05 16:01:21 +0900

    init commit

---
+++ src/main/java/nio/sample/javawebapp/JavaWebappApplication.java
@@ -0,0 +1,13 @@
+package nio.sample.javawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebappApplication.class, args);
+	}
+
+}
