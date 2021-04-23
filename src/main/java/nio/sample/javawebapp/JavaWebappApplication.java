435a17e9e9edcb8e9c570911f9579c0244858bcc
sehan <sehan.choi@gmail.com>
2021-04-23 10:03:44 +0900

    chore: gh action test

--- src/main/java/nio/sample/javawebapp/JavaWebappApplication.java
+++ src/main/java/nio/sample/javawebapp/JavaWebappApplication.java
@@ -3,6 +3,9 @@ package nio.sample.javawebapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 주석추가
+ */
 @SpringBootApplication
 public class JavaWebappApplication {
 
@@ -11,3 +14,4 @@ public class JavaWebappApplication {
 	}
 
 }
+
